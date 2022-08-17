a1bf913dfc279b7f0efca6878cf0a8687f52fd72
Roberto Reyes Perdomo <reyesperdomoroberto@gmail.com>
2022-08-17 09:12:17 -0700

    se modifico saludo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function data(){
     let nombre = document.getElementById("pet").value;
-    document.getElementById("saludo").innerHTML = "Nos encargaremos de " + nombre
+    document.getElementById("saludo").innerHTML = "Nos encargaremos de " + nombre + " como si estuviera en casa."
 
 }
