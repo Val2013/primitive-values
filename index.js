a8a604e19782e4e7eacc38f7ea42b60d89424ac0
Valeria Galicia <valeria.galicia@uabc.edu.mx>
2022-08-17 09:44:00 -0600

    esconderpag1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function data(){
     let nombre = document.getElementById("pet").value;
     document.getElementById("saludo").innerHTML = "Nos encargaremos de " + nombre + " como si estuviera en casa."
-
+    document.getElementById("pag1").className = "invisible"
 }
